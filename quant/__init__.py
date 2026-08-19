fd8ecdd7242b7c3e3de9f9e33b41896254464783
YASHUA <atomatom148@yahoo.com>
2026-08-19 15:36:07 -0500

    Phase A: initialize thin quant package

---
+++ quant/__init__.py
@@ -0,0 +1,11 @@
+"""ATOM V9 Thin quant package."""
+
+from .models import HORIZONS, ExactSixBundle, HorizonForecast, SetupState, Snapshot
+
+__all__ = [
+    "HORIZONS",
+    "ExactSixBundle",
+    "HorizonForecast",
+    "SetupState",
+    "Snapshot",
+]
